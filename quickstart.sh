1a85b61d0775ee5ff2b015b8cc018920e99dda66
Tsuyakashi <kashakdaniil@gmail.com>
2025-11-27 00:17:11 +0300

    images created

--- quickstart.sh
+++ quickstart.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+VM_NAME=ubuntu-noble
 KUBE_PASSWORD="root123"
-
+VM_IP=192.168.0.1
+APT_UPDATED=0
 function isRoot() {
 	if [ "${EUID}" -ne 0 ]; then
 		echo "You need to run this script as root"
@@ -9,58 +11,89 @@ function isRoot() {
 	fi
 }
 
-function checkXen () {
-    local apt_updated=0
-    if ! dpkg -s "xen-tools" &>/dev/null; then
-        echo "xen tools do not installed, so will be installed."
-        echo "installing packages"
-        sudo apt update &>/dev/null
-        apt_updated=1
-        sudo apt install -y xen-tools >/dev/null
+function getUbuntuVm(){
+    if ! virsh list | grep "$VM_NAME"; then
+        # add git exist check
+        [ ! -d kvm-on-machine ] && git clone https://github.com/Tsuyakashi/kvm-on-machine.git
+        
+        chmod +x ./kvm-on-machine/kvm-install.sh
+        sudo ./kvm-on-machine/kvm-install.sh --ubuntu --full
+
+        rm -rf kvm-on-machine
+    else
+        echo "VM already exists"
     fi
-    if ! dpkg -s "xen-hypervisor-4.17-amd64" &>/dev/null; then
-        echo "xen hypervisor do not installed, so will be installed."
-        echo "installing packages"
-        if [[ apt_updated != 1 ]]; then
-            sudo apt update &>/dev/null
-        fi
-        sudo apt install -y xen-hypervisor > /dev/null
+}
+
+function diskConfigure() {
+    if ! sudo vgs | grep vg0 &>/dev/null; then
+        sudo pvcreate /dev/vdb
+        sudo vgcreate vg0 /dev/vdb
     fi
- }
+}
 
 # xen-tools requires the classic .list file 
 function fixClassicAptList() {
-    if [[ "$1" == "--Undo" ]]; then
+    if [[ "$1" == "--undo" ]]; then
         sudo mv /etc/apt/sources.list \
         /etc/apt/sources.list.disabled
-        sudo apt update
+        [[ "$APT_UPDATED" != "1" ]] && sudo apt update && APT_UPDATED=1
+        sudo mv /etc/apt/sources.list.d/ubuntu.sources.disabled \
+            /etc/apt/sources.list.d/ubuntu.sources
         return 1
     fi
-    sudo tee /etc/apt/sources.list >/dev/null <<EOF
+    if [ ! -f /etc/apt/sources.list.d/ubuntu.sources.disabled ]; then
+        echo "Making classic apt sources list"
+        sudo tee /etc/apt/sources.list >/dev/null <<EOF
 deb http://archive.ubuntu.com/ubuntu noble main restricted universe multiverse
 deb http://archive.ubuntu.com/ubuntu noble-updates main restricted universe multiverse
 deb http://archive.ubuntu.com/ubuntu noble-backports main restricted universe multiverse
 deb http://security.ubuntu.com/ubuntu noble-security main restricted universe multiverse
 EOF
-    sudo mv /etc/apt/sources.list.d/ubuntu.sources \
-        /etc/apt/sources.list.d/ubuntu.sources.disabled
-    sudo apt update
-    # to Undo run with --Undo
+        sudo mv /etc/apt/sources.list.d/ubuntu.sources \
+            /etc/apt/sources.list.d/ubuntu.sources.disabled
+        echo "Updating apt packages..."
+        sudo apt update &>/dev/null
+        echo "Apt packages updated."
+        # to Undo run with --undo
+    fi
 }
+
+function checkXen () {
+    if ! dpkg -s "xen-tools" &>/dev/null; then
+        echo "xen-tools do not installed, so will be installed."
+        echo "Installing xen-tools packages..."
+        [[ "$APT_UPDATED" != "1" ]] && sudo apt update &>/dev/null && APT_UPDATED=1
+        sudo apt install -y xen-tools &>/dev/null
+        echo "xen-tools installed"
+    fi
+    if ! dpkg -s "xen-hypervisor-4.17-amd64" &>/dev/null; then
+        echo "xen-hypervisor do not installed, so will be installed."
+        echo "installing xen-hypervisor packages..."
+        [[ "$APT_UPDATED" != "1" ]] && sudo apt update &>/dev/null && APT_UPDATED=1
+        sudo apt install -y xen-hypervisor &>/dev/null
+        echo "xen-hypervisor installed."
+    fi
+ }
 
 function createImages() {
     local hostname=$1
     local ip=10.44.44.$2
     local mac=00:16:3e:44:44:$2
 
-    if ! -f /etc/xen/$hostname.pv; then
+    # --dir=/var/lib/xen/images
+    if [ ! -f /etc/xen/$hostname.pv ]; then
+        echo "Creating image for $hostname"
         xen-create-image \
         --hostname=$hostname --memory=2gb --vcpus=2 \
-        --dir=/var/lib/xen/images --ip=$ip --mac=$mac \
+        --lvm=vg0 --ip=$ip --mac=$mac \
         --pygrub --dist=bullseye --noswap --noaccounts \
         --noboot --nocopyhosts --extension=.pv \
         --fs=ext4 --genpass=0 --password=$KUBE_PASSWORD --nohosts \
         --bridge=xenlan44 --gateway=10.44.44.1 --netmask=255.255.255.0
+        echo "Image for $hostname created."
+    else
+        echo "$hostname.pv exists"
     fi
 }
 
@@ -75,7 +108,11 @@ function createSomeImages() {
 function addLocaltime() {
     local hostname=$1
 
-    echo "localtime = 1" | sudo tee /etc/xen/$hostname.pv >/dev/null
+    if ! cat /etc/xen/$hostname.pv | grep "localtime" &>/dev/null; then
+        echo "localtime = 1" | sudo tee -a /etc/xen/$hostname.pv >/dev/null
+    else
+        echo "localtime for $hostname added"
+    fi
 }
 
 function addSomeLocaltime() {
@@ -86,38 +123,28 @@ function addSomeLocaltime() {
     addLocaltime "worker02" 
 }
 
-function getUbuntuVm(){
-    # add git exist check
-    [ ! -d kvm-on-machine ] && git clone https://github.com/Tsuyakashi/kvm-on-machine.git
-    cd kvm-on-machine
-    chmod +x ./quickstart.sh
-    sudo ./quickstart.sh --ubuntu --full
-
-    cd ..
-}
 
 
-VM_NAME=ubuntu-noble
 
 function connectWithSSH() {
     if ! virsh domifaddr $VM_NAME | grep "ipv4" &>/dev/null; then
-        echo "vm did not start"
+        echo "VM did not start"
         exit
     else
-        VM_IP=$(virsh domifaddr $VM_NAME | awk '/ipv4/ { split($4, a, "/"); print a[1] }')
+        VM_IP=$(virsh domifaddr $VM_NAME | awk '/ipv4/ { split($4, a, "/"); print a[1] }') &>/dev/null
         while ! nc -z $VM_IP 22; do
             sleep 5
-            echo "did not accesible yet"
+            echo "VM did not accesible yet"
         done
     fi 
 
     
-    chmod 600 kvm-on-machine/keys/rsa.key
-    scp -i kvm-on-machine/keys/rsa.key \
+    chmod 600 ./keys/rsa.key
+    scp -i ./keys/rsa.key \
         -o StrictHostKeyChecking=accept-new \
         ./quickstart.sh \
         ubuntu@$VM_IP:~/ 
-    ssh -t -i kvm-on-machine/keys/rsa.key \
+    ssh -t -i ./keys/rsa.key \
         -o StrictHostKeyChecking=accept-new \
         ubuntu@$VM_IP \
         "IS_VM=1 sudo -E ./quickstart.sh" 
@@ -126,15 +153,13 @@ function connectWithSSH() {
 
 if [[ "$IS_VM" == "1" ]]; then
     isRoot
+    diskConfigure
     fixClassicAptList
     checkXen
     createSomeImages
-else
+    addSomeLocaltime
+else 
     isRoot
-
-    if ! virsh list | grep "$VM_NAME" &>/dev/null; then
-        getUbuntuVm
-    fi
-    
+    getUbuntuVm
     connectWithSSH
 fi
