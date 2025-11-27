61772926bd95b376dd43b379e4cabf6285072bd2
Tsuyakashi <kashakdaniil@gmail.com>
2025-11-27 03:08:41 +0300

    remove sudo

--- quickstart.sh
+++ quickstart.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
+set -e
+
 VM_NAME=ubuntu-noble
 KUBE_PASSWORD="root123"
 VM_IP=192.168.0.1
 APT_UPDATED=0
+
 function isRoot() {
 	if [ "${EUID}" -ne 0 ]; then
 		echo "You need to run this script as root"
 		exit 1
 	fi
 }
+isRoot
 
 function getUbuntuVm(){
-    if ! virsh list | grep "$VM_NAME"; then
+    if ! virsh list | grep -q "$VM_NAME"; then
         # add git exist check
         [ ! -d kvm-on-machine ] && git clone https://github.com/Tsuyakashi/kvm-on-machine.git
         
         chmod +x ./kvm-on-machine/kvm-install.sh
-        sudo ./kvm-on-machine/kvm-install.sh --ubuntu --full
+        ./kvm-on-machine/kvm-install.sh --ubuntu --full
 
         rm -rf kvm-on-machine
     else
@@ -26,34 +30,34 @@ function getUbuntuVm(){
 }
 
 function diskConfigure() {
-    if ! sudo vgs | grep vg0 &>/dev/null; then
-        sudo pvcreate /dev/vdb
-        sudo vgcreate vg0 /dev/vdb
+    if ! vgs | grep -q vg0; then
+        pvcreate /dev/vdb
+        vgcreate vg0 /dev/vdb
     fi
 }
 
 # xen-tools requires the classic .list file 
 function fixClassicAptList() {
     if [[ "$1" == "--undo" ]]; then
-        sudo mv /etc/apt/sources.list \
+        mv /etc/apt/sources.list \
         /etc/apt/sources.list.disabled
-        [[ "$APT_UPDATED" != "1" ]] && sudo apt update && APT_UPDATED=1
-        sudo mv /etc/apt/sources.list.d/ubuntu.sources.disabled \
+        [[ "$APT_UPDATED" != "1" ]] && apt update && APT_UPDATED=1
+        mv /etc/apt/sources.list.d/ubuntu.sources.disabled \
             /etc/apt/sources.list.d/ubuntu.sources
         return 1
     fi
     if [ ! -f /etc/apt/sources.list.d/ubuntu.sources.disabled ]; then
         echo "Making classic apt sources list"
-        sudo tee /etc/apt/sources.list >/dev/null <<EOF
+        tee /etc/apt/sources.list >/dev/null <<EOF
 deb http://archive.ubuntu.com/ubuntu noble main restricted universe multiverse
 deb http://archive.ubuntu.com/ubuntu noble-updates main restricted universe multiverse
 deb http://archive.ubuntu.com/ubuntu noble-backports main restricted universe multiverse
 deb http://security.ubuntu.com/ubuntu noble-security main restricted universe multiverse
 EOF
-        sudo mv /etc/apt/sources.list.d/ubuntu.sources \
+        mv /etc/apt/sources.list.d/ubuntu.sources \
             /etc/apt/sources.list.d/ubuntu.sources.disabled
         echo "Updating apt packages..."
-        sudo apt update &>/dev/null
+        apt update &>/dev/null
         echo "Apt packages updated."
         # to Undo run with --undo
     fi
@@ -63,15 +67,15 @@ function checkXen () {
     if ! dpkg -s "xen-tools" &>/dev/null; then
         echo "xen-tools do not installed, so will be installed."
         echo "Installing xen-tools packages..."
-        [[ "$APT_UPDATED" != "1" ]] && sudo apt update &>/dev/null && APT_UPDATED=1
-        sudo apt install -y xen-tools &>/dev/null
+        [[ "$APT_UPDATED" != "1" ]] && apt update &>/dev/null && APT_UPDATED=1
+        apt install -y xen-tools &>/dev/null
         echo "xen-tools installed"
     fi
     if ! dpkg -s "xen-hypervisor-4.17-amd64" &>/dev/null; then
         echo "xen-hypervisor do not installed, so will be installed."
         echo "installing xen-hypervisor packages..."
-        [[ "$APT_UPDATED" != "1" ]] && sudo apt update &>/dev/null && APT_UPDATED=1
-        sudo apt install -y xen-hypervisor &>/dev/null
+        [[ "$APT_UPDATED" != "1" ]] && apt update &>/dev/null && APT_UPDATED=1
+        apt install -y xen-hypervisor &>/dev/null
         echo "xen-hypervisor installed."
     fi
 }
@@ -123,8 +127,8 @@ function createSomeImages() {
 function addLocaltime() {
     local hostname=$1
 
-    if ! cat /etc/xen/$hostname.pv | grep "localtime" &>/dev/null; then
-        echo "localtime = 1" | sudo tee -a /etc/xen/$hostname.pv >/dev/null
+    if ! grep -q "localtime" /etc/xen/$hostname.pv; then
+        echo "localtime = 1" | tee -a /etc/xen/$hostname.pv >/dev/null
     else
         echo "localtime for $hostname added"
     fi
@@ -145,7 +149,7 @@ function etcdCreate() {
 }
 
 function connectWithSSH() {
-    if ! virsh domifaddr $VM_NAME | grep "ipv4" &>/dev/null; then
+    if ! virsh domifaddr $VM_NAME | grep -q "ipv4"; then
         echo "VM did not start"
         exit
     else
@@ -158,6 +162,10 @@ function connectWithSSH() {
 
     
     chmod 600 ./keys/rsa.key
+    if [ -n "$SUDO_USER" ] && [ "${EUID}" -eq 0 ]; then
+        chown $SUDO_USER:$SUDO_USER ./keys/rsa.key
+        chown $SUDO_USER:$SUDO_USER ./keys/rsa.key.pub
+    fi
     scp -i ./keys/rsa.key \
         -o StrictHostKeyChecking=accept-new \
         ./quickstart.sh \
@@ -176,7 +184,6 @@ function connectWithSSH() {
 }
 
 if [[ "$IS_VM" == "1" ]]; then
-    isRoot
     diskConfigure
     fixClassicAptList
     checkXen
@@ -185,7 +192,6 @@ if [[ "$IS_VM" == "1" ]]; then
     addSomeLocaltime
     etcdCreate
 else 
-    isRoot
     getUbuntuVm
     connectWithSSH
 fi
